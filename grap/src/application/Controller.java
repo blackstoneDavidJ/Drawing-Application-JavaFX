748b21a196aa41b105c61bb7622b45f055324c59
David Blackstone <62959991+blackstoneDavidJ@users.noreply.github.com>
2022-05-15 23:54:49 -0400

    Update: added tool radiobuttons, cursor updates

--- grap/src/application/Controller.java
+++ grap/src/application/Controller.java
@@ -3,26 +3,33 @@ package application;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 
 public class Controller implements Initializable
 {
-	
 	@FXML
-	private RadioButton rectangle;
+	private RadioButton rectangle, circle;
 	@FXML
-	private RadioButton circle;
+	private RadioButton drawPencil, eraserPencil, noTool;
 	@FXML
 	private RadioButton triangle;
 	@FXML
@@ -31,6 +38,10 @@ public class Controller implements Initializable
 	private Pane drawCanvas;
 	@FXML
 	private ColorPicker colorPicker;
+	@FXML
+	private Label lengthLabel, heightLabel, CircumferenceLabel;
+	@FXML
+	private TextField length, height, circumference;
 	private ArrayList<Node> shapes = new ArrayList<>();
 	private Color colorSelected;
 	private Shapes shapeToDraw;
@@ -53,35 +64,74 @@ public class Controller implements Initializable
 		});
 	}
 	
+	public void pencilTool()
+	{
+		if(drawPencil.isSelected())
+		{
+			Path path = new Path();
+			path.setStrokeWidth(10);
+			path.setStroke(colorSelected);
+			drawCanvas.setCursor(Cursor.CROSSHAIR);
+			drawCanvas.setOnDragDetected(e -> {
+				path.getElements().add(new MoveTo(e.getX(), e.getY()));
+				path.getElements().add(new LineTo(e.getX(), e.getY()));
+			});
+		}
+		
+		if(eraserPencil.isSelected())
+		{
+			drawCanvas.setCursor(Cursor.CROSSHAIR);
+		}
+	}
+	
 	public void setShape()
 	{
+		drawCanvas.setCursor(Cursor.DEFAULT);
 		if(rectangle.isSelected()) 
 		{
 			shapeToDraw = Shapes.RECTANGLE;
+			if(!length.isVisible())
+			{
+				length.setVisible(true);
+				height.setVisible(true);
+				lengthLabel.setVisible(true);
+				heightLabel.setVisible(true);
+			}
+			CircumferenceLabel.setVisible(false);
+			circumference.setVisible(false);
 		}
 		
 		if(circle.isSelected()) 
 		{
 			shapeToDraw = Shapes.CIRCLE;
+			if(!CircumferenceLabel.isVisible())
+			{
+				CircumferenceLabel.setVisible(true);
+				circumference.setVisible(true);
+			}
+			length.setVisible(false);
+			height.setVisible(false);
+			lengthLabel.setVisible(false);
+			heightLabel.setVisible(false);
 		}
 		
 		if(triangle.isSelected()) 
 		{
 			shapeToDraw = Shapes.TRIANGLE;
 		}
-		
 	}
 
 	public void createShape()
 	{
 		if(shapeToDraw == Shapes.CIRCLE)
 		{
-			Circle circle = new Circle(drawCanvas.getWidth()/2,drawCanvas.getHeight()/2, 100);
+			Circle circle = new Circle(drawCanvas.getWidth()/2,drawCanvas.getHeight()/2, Double.valueOf(circumference.getText().toString()));
 			circle.setFill(colorSelected);
 			shapes.add(circle);
 			drawCanvas.getChildren().add(circle);
 			circle.setOnMousePressed(e -> 
 			{
+				drawCanvas.setCursor(Cursor.OPEN_HAND);
 				if(e.isPrimaryButtonDown())
 				{
 					mouseAnchorX = e.getX();
@@ -98,15 +148,18 @@ public class Controller implements Initializable
 			{
 				if(e.isPrimaryButtonDown())
 				{
-					circle.setLayoutX(e.getSceneX() - mouseAnchorX);
-					circle.setLayoutY(e.getSceneY() - mouseAnchorY);
+					circle.setCenterX(e.getX());
+					circle.setCenterY(e.getY());
+					circle.setTranslateX(0);
+					circle.setTranslateY(0);
 				}
 			});
 		}
 		
 		if(shapeToDraw == Shapes.RECTANGLE)
 		{
-			Rectangle rec = new Rectangle(drawCanvas.getWidth()/2, drawCanvas.getHeight()/2, 100,100);
+			Rectangle rec = new Rectangle(drawCanvas.getWidth()/2, drawCanvas.getHeight()/2, Double.valueOf(length.getText().toString()),
+					Double.valueOf(height.getText().toString()));
 			rec.setFill(colorSelected);
 			drawCanvas.getChildren().add(rec);
 			rec.setOnMousePressed(e -> 
@@ -148,5 +201,11 @@ public class Controller implements Initializable
 		CIRCLE,
 		RECTANGLE,
 		TRIANGLE;
+	}
+	
+	public enum Tools
+	{
+		DRAWING,
+		ERASER;
 	}
 }
