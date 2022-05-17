07663941d09248ef2b128d8eedb2c643e09b5d15
David Blackstone <62959991+blackstoneDavidJ@users.noreply.github.com>
2022-05-17 01:28:12 -0400

    Update:
    Added Triangle support(inprogress), clear screen, save work buttons added, can adjust workspace size (bug: work off screen saves ) Bug with clicking radio buttons creates a shape

--- grap/src/application/Controller.java
+++ grap/src/application/Controller.java
@@ -14,20 +14,25 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.FileChooser;
@@ -36,6 +41,8 @@ public class Controller implements Initializable
 {
 	@FXML
 	private RadioButton rectangle, circle;
+	@FXML
+	private CheckBox collisions;
 	@FXML
 	private RadioButton drawPencil, eraserPencil, noTool;
 	@FXML
@@ -52,9 +59,11 @@ public class Controller implements Initializable
 	private TextField length, height, circumference, pencilThickness, windowLength, windowHeight;
 	private ArrayList<Node> nodesList = new ArrayList<>();
 	private Color colorSelected;
-	private Shapes shapeToDraw;
+	private Tools shapeToDraw;
 	private double mouseAnchorX;
 	private double mouseAnchorY;
+	private static final int BACKGROUND_WIDTH_CENTER = 1080/2;
+	private static final int BACKGROUND_HEIGHT_CENTER = 718/2;
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
@@ -67,6 +76,7 @@ public class Controller implements Initializable
 		drawCanvas.setLayoutY(drawCanvas.getHeight());
 		drawCanvas.setStyle("-fx-background-color: #FFFFFF");
 		backgroundCanvas.setStyle("-fx-background-color: #808080");
+		adjustWindow();
 		colorPicker.setOnAction(new EventHandler() 
 		{
 			@Override
@@ -75,16 +85,21 @@ public class Controller implements Initializable
 				colorSelected = colorPicker.getValue();					
 			}		
 		});
-		
 	}
 	
 	public void adjustWindow()
 	{
 		int windowL = Integer.valueOf(windowLength.getText().toString());
 		int windowH = Integer.valueOf(windowHeight.getText().toString());
-		
-		drawCanvas.setLayoutX((backgroundCanvas.getWidth()/2)-(windowL/2));
-		drawCanvas.setLayoutY((backgroundCanvas.getHeight()/2)-(windowH/2));
+		DropShadow drop = new DropShadow();
+		drop.setColor(Color.BLACK);
+		drop.setBlurType(BlurType.GAUSSIAN);
+		drop.setOffsetX(5);
+		drop.setOffsetY(5);
+		drop.setRadius(2);
+		drawCanvas.setEffect(drop);
+		drawCanvas.setLayoutX(BACKGROUND_WIDTH_CENTER-(windowL/2));
+		drawCanvas.setLayoutY(BACKGROUND_HEIGHT_CENTER-(windowH/2));
 		drawCanvas.setPrefWidth(windowL);
 		drawCanvas.setPrefHeight(windowH);
 		drawCanvas.setMaxWidth(windowL);
@@ -126,6 +141,166 @@ public class Controller implements Initializable
 	
 	public void pencilTool()
 	{
+		
+	}
+	
+	public void setTool()
+	{
+		drawCanvas.setCursor(Cursor.DEFAULT);
+		if(rectangle.isSelected()) 
+		{
+			shapeToDraw = Tools.RECTANGLE;
+			if(!length.isVisible())
+			{
+				length.setVisible(true);
+				height.setVisible(true);
+				lengthLabel.setVisible(true);
+				heightLabel.setVisible(true);
+			}
+			CircumferenceLabel.setVisible(false);
+			circumference.setVisible(false);
+		}
+		
+		else if(circle.isSelected()) 
+		{
+			shapeToDraw = Tools.CIRCLE;
+			if(!CircumferenceLabel.isVisible())
+			{
+				CircumferenceLabel.setVisible(true);
+				circumference.setVisible(true);
+			}
+			length.setVisible(false);
+			height.setVisible(false);
+			lengthLabel.setVisible(false);
+			heightLabel.setVisible(false);
+		}
+		
+		else if(triangle.isSelected()) 
+		{
+			shapeToDraw = Tools.TRIANGLE;
+		}		
+	}
+
+	public void createShape()
+	{
+		if(shapeToDraw == Tools.CIRCLE)
+		{
+			Circle circle = new Circle(drawCanvas.getWidth()/2,drawCanvas.getHeight()/2,
+					Double.valueOf(circumference.getText().toString()));
+			circle.setFill(colorSelected);
+			nodesList.add(circle);
+			drawCanvas.getChildren().add(circle);
+			circle.setOnMousePressed(e -> 
+			{
+				drawCanvas.setCursor(Cursor.OPEN_HAND);
+				if(e.isPrimaryButtonDown())
+				{
+					mouseAnchorX = e.getX();
+					mouseAnchorY = e.getY();
+				}
+				
+				if(e.isSecondaryButtonDown())
+				{
+					drawCanvas.getChildren().remove(circle);
+					nodesList.remove(circle);
+				}
+			});
+			
+			circle.setOnMouseDragged(e -> 
+			{
+				if(e.isPrimaryButtonDown())
+				{
+					circle.setCenterX(e.getX());
+					circle.setCenterY(e.getY());
+					circle.setTranslateX(0);
+					circle.setTranslateY(0);
+					
+					//Circle collisions with screen
+					if(collisions.isSelected())
+					{
+						if(circle.getCenterX()+circle.getRadius() > drawCanvas.getWidth())
+						{
+							circle.setCenterX(drawCanvas.getWidth()-circle.getRadius());
+						}
+						
+						if(circle.getCenterX()-circle.getRadius() < 0)
+						{
+							circle.setCenterX(0+circle.getRadius());
+						}
+						
+						if(circle.getCenterY()+circle.getRadius() > drawCanvas.getHeight())
+						{
+							circle.setCenterY(drawCanvas.getHeight()-circle.getRadius());
+						}
+						
+						if(circle.getCenterY()-circle.getRadius() < 0)
+						{
+							circle.setCenterY(0+circle.getRadius());
+						}
+					}
+				}
+			});
+		}
+		
+		if(shapeToDraw == Tools.RECTANGLE)
+		{
+			Rectangle rec = new Rectangle(drawCanvas.getWidth()/2, drawCanvas.getHeight()/2, Double.valueOf(length.getText().toString()),
+					Double.valueOf(height.getText().toString()));
+			rec.setFill(colorSelected);
+			drawCanvas.getChildren().add(rec);	
+			nodesList.add(rec);
+			final Delta dragDelta = new Delta();
+	        rec.setOnMousePressed(new EventHandler<MouseEvent>() {
+	            @Override
+	            public void handle(MouseEvent mouseEvent) 
+	            {
+	            	if(mouseEvent.isPrimaryButtonDown())
+	            	{
+	            		// record a delta distance for the drag and drop operation.
+	            		dragDelta.x = rec.getTranslateX() - mouseEvent.getSceneX();
+	            		dragDelta.y = rec.getTranslateY() - mouseEvent.getSceneY();
+	            		rec.setCursor(Cursor.OPEN_HAND);
+	            	}
+	            	
+	            	else
+	            	{
+	            		drawCanvas.getChildren().remove(rec);
+	            		nodesList.remove(rec);
+	            	}
+	            }
+	        });
+	        rec.setOnMouseReleased(new EventHandler<MouseEvent>() {
+	            @Override
+	            public void handle(MouseEvent mouseEvent) {
+	            }
+	        });
+	        rec.setOnMouseDragged(new EventHandler<MouseEvent>() {
+	            @Override
+	            public void handle(MouseEvent mouseEvent) {
+
+	                rec.setTranslateX(mouseEvent.getSceneX() + dragDelta.x);
+	                rec.setTranslateY(mouseEvent.getSceneY() + dragDelta.y);
+	                if(rec.getX() +rec.getWidth() > drawCanvas.getWidth())
+	                {
+	                	rec.setX(mouseAnchorX);
+	                }
+	                
+	                if(rec.getY() + rec.getHeight() > drawCanvas.getHeight())
+	                {
+	                	rec.setTranslateX(drawCanvas.getHeight()-rec.getHeight());
+	                }
+	            }
+	        });
+		}
+		
+		if(shapeToDraw == Tools.TRIANGLE)
+		{
+			Point2D center = new Point2D(drawCanvas.getWidth()/2, drawCanvas.getHeight()/2);
+			Polygon triangle = createTriangle(center,10,90);
+			drawCanvas.getChildren().add(triangle);
+			nodesList.add(triangle);
+		}
+		
 		Canvas canvas = new Canvas(drawCanvas.getWidth(), drawCanvas.getHeight());
 		if(drawPencil.isSelected())
 		{
@@ -151,10 +326,14 @@ public class Controller implements Initializable
 	                new EventHandler<MouseEvent>(){
 
 	            @Override
-	            public void handle(MouseEvent event) {
-	            	
-	            	//handle Pencil Collisions with border
-	                graphicsContext.lineTo(event.getX(), event.getY());
+	            public void handle(MouseEvent event) 
+	            {	
+	            	checkPencilCollisions(graphicsContext, event);         
+	            }
+
+				private void checkPencilCollisions(GraphicsContext graphicsContext, MouseEvent event)
+				{
+					graphicsContext.lineTo(event.getX(), event.getY());
 	                if(event.getX()> canvas.getWidth())
 	                {
 	                	graphicsContext.lineTo(canvas.getWidth()-graphicsContext.getLineWidth()/2,
@@ -177,7 +356,7 @@ public class Controller implements Initializable
 	                			0+graphicsContext.getLineWidth()/2);
 	                }
 	                graphicsContext.stroke();
-	            }
+				}
 	        });
 
 	        canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, 
@@ -238,167 +417,27 @@ public class Controller implements Initializable
 		}
 	}
 	
-	public void setShape()
+	private Polygon createTriangle(Point2D origin, double length, double angle)
 	{
-		drawCanvas.setCursor(Cursor.DEFAULT);
-		if(rectangle.isSelected()) 
-		{
-			shapeToDraw = Shapes.RECTANGLE;
-			if(!length.isVisible())
-			{
-				length.setVisible(true);
-				height.setVisible(true);
-				lengthLabel.setVisible(true);
-				heightLabel.setVisible(true);
-			}
-			CircumferenceLabel.setVisible(false);
-			circumference.setVisible(false);
-		}
-		
-		if(circle.isSelected()) 
-		{
-			shapeToDraw = Shapes.CIRCLE;
-			if(!CircumferenceLabel.isVisible())
-			{
-				CircumferenceLabel.setVisible(true);
-				circumference.setVisible(true);
-			}
-			length.setVisible(false);
-			height.setVisible(false);
-			lengthLabel.setVisible(false);
-			heightLabel.setVisible(false);
-		}
-		
-		if(triangle.isSelected()) 
-		{
-			shapeToDraw = Shapes.TRIANGLE;
-		}
-	}
-
-	public void createShape()
-	{
-		if(shapeToDraw == Shapes.CIRCLE)
-		{
-			Circle circle = new Circle(drawCanvas.getWidth()/2,drawCanvas.getHeight()/2,
-					Double.valueOf(circumference.getText().toString()));
-			circle.setFill(colorSelected);
-			nodesList.add(circle);
-			drawCanvas.getChildren().add(circle);
-			circle.setOnMousePressed(e -> 
-			{
-				drawCanvas.setCursor(Cursor.OPEN_HAND);
-				if(e.isPrimaryButtonDown())
-				{
-					mouseAnchorX = e.getX();
-					mouseAnchorY = e.getY();
-				}
-				
-				if(e.isSecondaryButtonDown())
-				{
-					drawCanvas.getChildren().remove(circle);
-					nodesList.remove(circle);
-				}
-			});
-			
-			circle.setOnMouseDragged(e -> 
-			{
-				if(e.isPrimaryButtonDown())
-				{
-					circle.setCenterX(e.getX());
-					circle.setCenterY(e.getY());
-					circle.setTranslateX(0);
-					circle.setTranslateY(0);
-					
-					//Circle collisions with screen
-					if(circle.getCenterX()+circle.getRadius() > drawCanvas.getWidth())
-					{
-						circle.setCenterX(drawCanvas.getWidth()-circle.getRadius());
-					}
-					
-					if(circle.getCenterX()-circle.getRadius() < 0)
-					{
-						circle.setCenterX(0+circle.getRadius());
-					}
-					
-					if(circle.getCenterY()+circle.getRadius() > drawCanvas.getHeight())
-					{
-						circle.setCenterY(drawCanvas.getHeight()-circle.getRadius());
-					}
-					
-					if(circle.getCenterY()-circle.getRadius() < 0)
-					{
-						circle.setCenterY(0+circle.getRadius());
-					}
-				}
-			});
-		}
-		
-		if(shapeToDraw == Shapes.RECTANGLE)
-		{
-			Rectangle rec = new Rectangle(drawCanvas.getWidth()/2, drawCanvas.getHeight()/2, Double.valueOf(length.getText().toString()),
-					Double.valueOf(height.getText().toString()));
-			rec.setFill(colorSelected);
-			drawCanvas.getChildren().add(rec);	
-			nodesList.add(rec);
-			final Delta dragDelta = new Delta();
-	        rec.setOnMousePressed(new EventHandler<MouseEvent>() {
-	            @Override
-	            public void handle(MouseEvent mouseEvent) 
-	            {
-	            	if(mouseEvent.isPrimaryButtonDown())
-	            	{
-	            		// record a delta distance for the drag and drop operation.
-	            		dragDelta.x = rec.getTranslateX() - mouseEvent.getSceneX();
-	            		dragDelta.y = rec.getTranslateY() - mouseEvent.getSceneY();
-	            		rec.setCursor(Cursor.OPEN_HAND);
-	            	}
-	            	
-	            	else
-	            	{
-	            		drawCanvas.getChildren().remove(rec);
-	            		nodesList.remove(rec);
-	            	}
-	            }
-	        });
-	        rec.setOnMouseReleased(new EventHandler<MouseEvent>() {
-	            @Override
-	            public void handle(MouseEvent mouseEvent) {
-	            }
-	        });
-	        rec.setOnMouseDragged(new EventHandler<MouseEvent>() {
-	            @Override
-	            public void handle(MouseEvent mouseEvent) {
-
-	                rec.setTranslateX(mouseEvent.getSceneX() + dragDelta.x);
-	                rec.setTranslateY(mouseEvent.getSceneY() + dragDelta.y);
-	                if(rec.getX() +rec.getWidth() > drawCanvas.getWidth())
-	                {
-	                	rec.setX(mouseAnchorX);
-	                }
-	                
-	                if(rec.getY() + rec.getHeight() > drawCanvas.getHeight())
-	                {
-	                	rec.setTranslateX(drawCanvas.getHeight()-rec.getHeight());
-	                }
-	                //checkBounds(rec);
-
-	            }
-	        });
-		}
-		
-		if(shapeToDraw == Shapes.TRIANGLE)
-		{
-			
-		}
+	    Polygon fovTriangle = new Polygon(0d, 0d,(length * Math.tan(angle)), -length,
+	            -(length * Math.tan(angle)), -length
+	    );
+	    
+	    fovTriangle.setLayoutX(origin.getX());
+	    fovTriangle.setLayoutY(origin.getY());
+	    fovTriangle.setFill(colorSelected);
+	    return fovTriangle;
 	}
 	
 	class Delta  { double x, y;}
 	
-	public enum Shapes
+	private enum Tools
 	{
 		NONE,
 		CIRCLE,
 		RECTANGLE,
-		TRIANGLE;
+		TRIANGLE,
+		PENCIL,
+		ERASER;
 	}
 }
